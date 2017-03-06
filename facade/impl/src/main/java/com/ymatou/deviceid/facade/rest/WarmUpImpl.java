5b30ac36070117e56920215283b6040c4a502e01
ÍõÐñ¶« <wangxudong@ymatou.com>
2017-03-06 16:44:32 +0800

    change dubbo serialization to hession

--- facade/impl/src/main/java/com/ymatou/deviceid/facade/rest/WarmUpImpl.java
+++ facade/impl/src/main/java/com/ymatou/deviceid/facade/rest/WarmUpImpl.java
@@ -1,9 +1,13 @@
 package com.ymatou.deviceid.facade.rest;
 
-import com.alibaba.dubbo.config.annotation.Service;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
 import org.springframework.stereotype.Component;
 
-import javax.ws.rs.*;
+import com.alibaba.dubbo.config.annotation.Service;
+import com.ymatou.deviceid.infrastructure.util.NetUtil;
 
 /**
  * Created by liangzhonghua on 2016/9/26.
@@ -19,6 +23,8 @@ public class WarmUpImpl implements WarmUp {
     @Path("/version")
     public String version() {
         return "{"
+                + "\"port\":9900,"
+                + "\"ip\":\"" + NetUtil.getHostIp() + "\","
                 + "\"1.0.0\":\"2016-09-30.1 created\","
                 + "\"1.0.1\":\"2016-10-09.1 get method add param type\","
                 + "\"1.0.2\":\"2016-10-09.3 add update method\","
@@ -29,7 +35,8 @@ public class WarmUpImpl implements WarmUp {
                 + "\"1.0.7\":\"2016-11-09.1 set signVerified ok before 2016-11-01\","
                 + "\"1.0.8\":\"2016-11-16.3 modify updateDeviceId api when deviceid exist and userid not equals req\","
                 + "\"1.0.9\":\"2016-12-02.1 modify save api ensure idempotent\","
-                + "\"1.0.10\":\"2017-02-21.1 add getUserIdList api\""
+                + "\"1.0.10\":\"2017-02-21.1 add getUserIdList api\","
+                + "\"1.0.11\":\"2017-03-06.1 change dubbo serialization to hession\""
                 + "}";
     }
 
